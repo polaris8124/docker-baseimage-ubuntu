eca1e6935704222c0a0a72cf8292261eab743c7b
mikael <mikael@polaris.ovh>
2025-02-16 16:22:49 +0100

    gitea

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-docker build --pull --no-cache -t registry.polaris.ovh/image-base-ubuntu:polaris-noble-latest .
-docker push registry.polaris.ovh/image-base-ubuntu:polaris-noble-latest
+docker build --pull --no-cache -t gitea.polaris.ovh/polaris/image-base-ubuntu:polaris-noble-latest .
+docker push gitea.polaris.ovh/polaris/image-base-ubuntu:polaris-noble-latest
